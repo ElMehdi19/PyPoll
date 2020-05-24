d670ccbe8d4fa8c3fcc1fe15e42ef8a03d806ba7
El Mehdi Rami <lbiggboss19@gmail.com>
2020-05-24 04:17:11 +0000

    Update config.py

--- app/FlaskApp/config.py
+++ app/FlaskApp/config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('POLLAPP_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('POLLAPP_SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('POLLAPP_MAIL_USER')
-    MAIL_PASSWORD = os.environ.get('POLLAPP_MAIL_PWD')
+    MAIL_USERNAME = os.environ.get('MAIL_USER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PWD')
     MAIL_SUPPRESS_SEND = False
